e41b2dd8734849ae768fcd7fdd2fd9e70a007062
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-12 10:58:11 -0600

    practice

--- server.js
+++ server.js
@@ -1,2 +1,44 @@
+//============Dependencies
+
 var express = require('express')
 var bodyParser = require('body-parser')
+
+
+//============intialization of server
+
+var app = express(); // <-- turning on expresss
+
+app.use(bodyParser.json())
+
+var user = require('./user.js')
+
+
+
+app.get('/getname', function(req, res){
+    res.status(200).json(user.name);
+})
+
+app.put('/updatename', function(req, res) {
+  user.name = req.body.name
+  res.status(200).json(user.name);
+})
+
+app.post('/addcandy', function(req, res) {
+    user.favoriteCandy.push(req.body.candy);
+    res.status(200).json(user.favoriteCandy);
+})
+
+
+
+var port = 3030;
+app.listen(port, function() {
+    console.log("It's over: " + port);
+})
+
+
+// NOTE: Req = Request: coming from the browser is a Request
+// NOTE: Res = Response: sending things back
+// NOTE: PUT: UPDATE CONTENT
+// NOTE: GET: REQUEST INFORMATION
+// NOTE: POST: ADD NEW INFORMATION
+// NOTE: D
---
+++ user.js
@@ -0,0 +1,9 @@
+var user = {
+  name: 'Dallin',
+  location: 'Alpine',
+  phoneNumber: '8015804155',
+  email: '@gmail.com',
+  favoriteCandy: ['snickers', 'skittles', 'kit-kat']
+}
+
+module.exports = user;
